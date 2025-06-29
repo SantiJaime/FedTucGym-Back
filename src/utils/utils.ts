b4b6dd0f645610b118942c991fe0dcb22869c449
SantiJaime <santij03@gmail.com>
2025-06-29 17:40:10 -0300

    creo query dinamica para obtener alumnos, modifico mensaje de refreshauth

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,5 @@
 import type { ZodIssue } from "zod";
+import type { MembersFilter } from "../schemas/members.shema";
 
 interface Dates {
   startDate: string;
@@ -16,4 +17,38 @@ export const subtractDays = (dateStr: string, days: number = 10): string => {
 
 export const parseErrors = (errors: ZodIssue[]) => {
   return errors.map((error) => `${error.path}: ${error.message}`).join(" | ");
+};
+export const toNumberOrUndefined = (val: any) =>
+  val === undefined ? undefined : Number(val);
+
+export const parseFilters = (filters: MembersFilter, gymId: number) => {
+  let baseQuery = "SELECT * FROM members_view WHERE id_gym = $1";
+  let values: any[] = [gymId];
+  let conditions: string[] = [];
+
+  if (filters.full_name) {
+    conditions.push(`full_name ILIKE $${values.length + 1}`);
+    values.push(`%${filters.full_name}%`);
+  }
+
+  if (filters.id_category) {
+    conditions.push(`id_category = $${values.length + 1}`);
+    values.push(filters.id_category);
+  }
+
+  if (filters.id_level) {
+    conditions.push(`id_level = $${values.length + 1}`);
+    values.push(filters.id_level);
+  }
+  if (filters.dni) {
+    conditions.push(`dni = $${values.length + 1}`);
+    values.push(filters.dni);
+  }
+
+  const query =
+    values.length > 1
+      ? baseQuery + " AND " + conditions.join(" AND ") + " ORDER BY id ASC"
+      : baseQuery;
+  console.log(query, values);
+  return { query, values };
 };
